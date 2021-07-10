784e7d64ed8d150d43229c525d546b4e5639effe
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-07-10 18:28:55 +0200

    Docker setup. Simple filesystem session test.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from flask import Flask
+from datetime import timedelta
+
+from flask import Flask, session
+from flask_session import Session
 
 app = Flask(__name__)
 
-### TODO Add:
-# Rest API endpoints
-# Session management
-# API key authentication
+# TODO read from env
+app.config['SECRET_KEY'] = '0f54d116-ae42-469e-8cd7-19496cb338dc'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=5)
+app.config['SESSION_FILE_THRESHOLD'] = 50
+sess = Session(app)
 
 
-@app.route('/')
-def home():
-    return 'Skeleton code for a simple Python/Flask application'
+@app.route('/set/<value>')
+def set_session(value):
+    session['value'] = value
+    return f'The value you set is: { value }'
+
+
+@app.route('/get')
+def get_session():
+    return f'The value in the session is: { session.get("value") }'
 
 
 if __name__ == '__main__':
