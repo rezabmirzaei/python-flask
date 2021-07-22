8c1b1133ab7d24c68a56c9ee72a34cdf01996c77
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-07-23 00:34:22 +0200

    Handle query params

--- apis/search_ns.py
+++ apis/search_ns.py
@@ -1,11 +1,20 @@
-from flask_restx import Namespace, Resource
+from flask import request
+from flask_restx import Namespace, Resource, reqparse
 
 ns = Namespace('search', description='Search endpoints')
+
+parser = reqparse.RequestParser()
+parser.add_argument('q', type=str, required=True,
+                    help='The value to search for')
 
 
 @ns.route('/')
 @ns.param('q')
 class Search(Resource):
+
+    @ns.expect(parser)
     def get(self):
         '''Fetch search result'''
+        if request.args.get('q') is None:
+            ns.abort(400)
         return [{'key': 'value'}, {'key': 'value'}]
